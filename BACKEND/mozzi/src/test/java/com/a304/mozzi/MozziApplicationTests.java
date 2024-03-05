883299cbdd5353ddfe9f8a2444247a98d04322c4
sangbumlikeagod <oironese@naver.com>
2024-03-06 00:07:24 +0900

    feat : 백엔드 시작

---
+++ BACKEND/mozzi/src/test/java/com/a304/mozzi/MozziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a304.mozzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
