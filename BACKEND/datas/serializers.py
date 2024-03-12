bb9265a71517537aaa4323d477a2eb2859a9324f
김영빈 <bigstar96115@hanmail.net>
2024-03-12 13:38:37 +0900

    Deleted BACKEND/datas/__pycache__/__init__.cpython-39.pyc, BACKEND/datas/__pycache__/admin.cpython-39.pyc, BACKEND/datas/__pycache__/apps.cpython-39.pyc, BACKEND/datas/__pycache__/models.cpython-39.pyc, BACKEND/datas/__pycache__/serializers.cpython-39.pyc, BACKEND/datas/__pycache__/urls.cpython-39.pyc, BACKEND/datas/__pycache__/views.cpython-39.pyc, BACKEND/datas/migrations/__pycache__/0001_initial.cpython-39.pyc, BACKEND/datas/migrations/__pycache__/__init__.cpython-39.pyc, BACKEND/datas/migrations/0001_initial.py, BACKEND/datas/migrations/__init__.py, BACKEND/datas/__init__.py, BACKEND/datas/admin.py, BACKEND/datas/apps.py, BACKEND/datas/models.py, BACKEND/datas/serializers.py, BACKEND/datas/tests.py, BACKEND/datas/urls.py, BACKEND/datas/views.py, BACKEND/mozzi_django/__pycache__/__init__.cpython-39.pyc, BACKEND/mozzi_django/__pycache__/settings.cpython-39.pyc, BACKEND/mozzi_django/__pycache__/urls.cpython-39.pyc, BACKEND/mozzi_django/__pycache__/wsgi.cpython-39.pyc, BACKEND/mozzi_django/__init__.py, BACKEND/mozzi_django/asgi.py, BACKEND/mozzi_django/settings.py, BACKEND/mozzi_django/urls.py, BACKEND/mozzi_django/wsgi.py, BACKEND/requirements.txt, BACKEND/my_mongodb_database.mongo_food.json, BACKEND/manage.py

--- BACKEND/datas/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Food
-from rest_framework import serializers
-
-class FoodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Food
-        fields = '__all__'
