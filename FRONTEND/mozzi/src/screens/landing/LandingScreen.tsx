6282331afee44a0bd609a2cc0cf0b5be1e3e71ec
geumhyun lee <goldbutnew@gmail.com>
2024-03-13 12:53:25 +0900

    feat: add animation

--- FRONTEND/mozzi/src/screens/landing/LandingScreen.tsx
+++ FRONTEND/mozzi/src/screens/landing/LandingScreen.tsx
@@ -1,91 +1,148 @@
-import { View, Text, Button, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import styled from 'styled-components/native'
+import React, { useEffect, useRef } from 'react';
+import { Animated, TouchableOpacity, Image } from 'react-native';
+import styled from 'styled-components/native';
 
-import kakao from '../../assets/landing/kakao-login-icon.png'
-import wave from '../../assets/landing/wave-bg.png'
-import icon1 from '../../assets/landing/icon1.png'
-import icon2 from '../../assets/landing/icon2.png'
-import icon3 from '../../assets/landing/icon3.png'
-import icon4 from '../../assets/landing/icon4.png'
-import icon5 from '../../assets/landing/icon5.png'
-import icon6 from '../../assets/landing/icon6.png'
-import icon7 from '../../assets/landing/icon7.png'
-import icon8 from '../../assets/landing/icon8.png'
-import icon9 from '../../assets/landing/icon9.png'
+import kakao from '../../assets/landing/kakao-login-icon.png';
+import wave from '../../assets/landing/wave-bg.png';
+
+const icons = [
+  require('../../assets/landing/icon1.png'),
+  require('../../assets/landing/icon2.png'),
+  require('../../assets/landing/icon3.png'),
+  require('../../assets/landing/icon4.png'),
+  require('../../assets/landing/icon5.png'),
+  require('../../assets/landing/icon6.png'),
+  require('../../assets/landing/icon7.png'),
+  require('../../assets/landing/icon8.png'),
+  require('../../assets/landing/icon9.png'),
+];
 
 const Container = styled.View`
   flex: 1;
-  position: relative;
   background-color: #FFFDD4;
-`
-const BgImg = styled.Image` 
-  position: relative;
+`;
+
+const AnimatedBgImg = Animated.createAnimatedComponent(styled.Image`
+  position: absolute;
   width: 100%;
   height: 80%;
-  top: -20%;
-`
+  top: -15%;
+`);
 
-const FoodIcon = styled.Image`
+const AnimatedFoodIcon = Animated.createAnimatedComponent(styled.Image`
   position: absolute;
   width: ${props => props.width}px;
   height: ${props => props.height}px;
   top: ${props => props.top}px;
-  left: ${props => props.left ? props.left + 'px' : undefined};
-  right: ${props => props.right ? props.right + 'px' : undefined};
-`
+  left: ${props => props.left}px;
+`);
 
 const TitleContainer = styled.View`
   position: absolute;
   margin-top: 20%;
   padding-left: 15%;
-`
+`;
+
 const SmallTitle = styled.Text`
-  font-size: 48;
-`
+  font-size: 48px;
+`;
+
 const BigTitle = styled.Text`
-  font-size: 96;
-`
+  font-size: 96px;
+`;
 
 const LoginContainer = styled.View`
-  margin-top: 50px;
-  display: flex;
+  position: absolute;
+  bottom: 22%;
+  width: 100%;
   align-items: center;
-`
+`;
+
+const AnimatedLoginButton = Animated.createAnimatedComponent(TouchableOpacity);
 
 function LandingScreen() {
-  
+  // const fadeAnims = useRef(
+  //   Array.from({ length: 9 }, () => new Animated.Value(0))
+  // ).current;
+
+  // 각 아이콘과 로그인 버튼에 대한 페이드인 애니메이션 값
+  const fadeAnims = useRef(icons.map(() => new Animated.Value(0))).current;
+  const loginFadeAnim = useRef(new Animated.Value(0)).current;
+
+  // 페이드인 애니메이션을 시작하는 함수
+  const fadeIn = (animValue, delay) => {
+    Animated.timing(animValue, {
+      toValue: 1,
+      duration: 1000,
+      delay,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    // 아이콘 애니메이션
+    fadeAnims.forEach((anim, index) => {
+      fadeIn(anim, index * 500); // 각 아이콘이 0.5초 간격으로 나타나게 설정
+    });
+
+    // 모든 아이콘 애니메이션 후 로그인 버튼 애니메이션 시작
+    const lastIconDelay = icons.length * 500;
+    fadeIn(loginFadeAnim, lastIconDelay);
+  }, []);
+
   const kakaoLogin = () => {
-    console.log('카카오 로그인으로 대체해')
-  }
+    console.log('카카오 로그인으로 대체해');
+  };
+
+  // 위치와 크기 정보
+  const positions = [
+    { width: 53, height: 53, top: 334.82, left: 77 },
+    { width: 64, height: 64, top: 361.72, left: 179 },
+    { width: 65, height: 65, top: 270, left: 286 },
+    { width: 66, height: 66, top: 442.42, left: 44 },
+    { width: 50, height: 50, top: 529.09, left: 190 },
+    { width: 67, height: 67, top: 467.32, left: 290 },
+    { width: 58, height: 58, top: 765.21, left: 53 },
+    { width: 65, height: 65, top: 707.43, left: 149 },
+    { width: 80, height: 80, top: 742.3, left: 277 },
+  ];
 
   return (
     <Container>
-
-      <BgImg source={wave} />
+      <AnimatedBgImg source={wave} style={{ opacity: fadeAnims[0] }} />
       <TitleContainer>
         <SmallTitle>오늘 모 먹찌?</SmallTitle>
-        <BigTitle>모찌</BigTitle>        
+        <BigTitle>모찌</BigTitle>
       </TitleContainer>
 
-      <FoodIcon source={icon1} width={53} height={53} top={334.82} left={77}/>
-      <FoodIcon source={icon2} width={64} height={64} top={361.72} left={179}/>
-      <FoodIcon source={icon3} width={65} height={65} top={270} left={286}/>
-      <FoodIcon source={icon4} width={66} height={66} top={442.42} left={44}/>
-      <FoodIcon source={icon5} width={50} height={50} top={529.09} left={190}/>
-      <FoodIcon source={icon6} width={67} height={67} top={467.32} left={290}/>
-      <FoodIcon source={icon7} width={58} height={58} top={765.21} left={53}/>
-      <FoodIcon source={icon8} width={65} height={65} top={707.43} left={149}/>
-      <FoodIcon source={icon9} width={80} height={80} top={742.3} left={277}/>
+      {/* 아이콘들을 순회하며 AnimatedFoodIcon 컴포넌트 생성 */}
+      {positions.map((pos, index) => (
+        <AnimatedFoodIcon
+          key={index}
+          source={icons[index]}
+          style={[
+            {
+              width: pos.width,
+              height: pos.height,
+              top: pos.top,
+              left: pos.left,
+              opacity: fadeAnims[index],
+            },
+          ]}
+        />
+      ))}
 
       <LoginContainer>
-        <TouchableOpacity onPress={kakaoLogin}>
+      {/* 로그인 버튼 */}
+        <AnimatedLoginButton
+          style={{ opacity: loginFadeAnim }}
+          onPress={kakaoLogin}
+        >
           <Image source={kakao} />
-        </TouchableOpacity>
+        </AnimatedLoginButton>
       </LoginContainer>
-
     </Container>
-  )
+  );
 }
 
-export default LandingScreen
+export default LandingScreen;
