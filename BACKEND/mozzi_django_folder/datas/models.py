4e9af8d4fa1fba5e209e197acac9e0f7a9ced4b1
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-03-14 19:03:03 +0900

    feat: Add ingredient to mysql

--- BACKEND/mozzi_django_folder/datas/models.py
+++ BACKEND/mozzi_django_folder/datas/models.py
@@ -40,3 +40,8 @@ class Category(models.Model):
     category_name = models.CharField(max_length=20)
     category_pic = models.CharField(max_length=200)
 
+class Ingredient(models.Model):
+    ingredient_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.ingredient_name
