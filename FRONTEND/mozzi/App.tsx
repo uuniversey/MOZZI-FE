38badc8b0070b29ba493f542cc439050cb87d0a1
nyk <ryk365@naver.com>
2024-03-19 16:18:17 +0900

    merge: develop/recap
    Change-Id: I8f2b09bfdc0448ba10c8753cdc4946f0d0e23ca1

--- FRONTEND/mozzi/App.tsx
+++ FRONTEND/mozzi/App.tsx
@@ -55,7 +55,7 @@ const App: React.FC = () => {
   return (
     <>
       
-      {isLogin ? (
+      {!isLogin ? (
         <NavigationContainer>
           <Tab.Navigator
             screenOptions={({ route }) => ({
