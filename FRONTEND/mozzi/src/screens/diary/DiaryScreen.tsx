4241cb8358a674ddd3640da09f9853c1097e5d2b
mm9326 <mm9326@naver.com>
2024-03-22 17:29:36 +0900

    feat: diary axios complete

--- FRONTEND/mozzi/src/screens/diary/DiaryScreen.tsx
+++ FRONTEND/mozzi/src/screens/diary/DiaryScreen.tsx
@@ -1,11 +1,13 @@
 import { View, Text, Button, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons'
 import styled from 'styled-components/native'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useNavigation } from '@react-navigation/native'
-import { Calendar, DateData } from 'react-native-calendars'
+import { Calendar } from 'react-native-calendars'
+
+import useDiaryStore from '../../store/DiaryStore'
 
 interface DayStyleProps {
   isSelected: boolean;
@@ -76,60 +78,85 @@ const CalendarImage = styled.Image`
 `
 
 function DiaryScreen () {
-
   const navigation = useNavigation()
+  const { calendarData, getCalendar } = useDiaryStore()
 
-  const moveDiaryDetail = (date: Date) => {
-    navigation.navigate("DiaryDetail", {date: date})
+  useEffect (() => {
+    // 컴포넌트 마운트 시 현재 달의 데이터를 불러옵니다.
+    const today = new Date()
+    console.log(today)
+    const year = today.getFullYear().toString()
+    const month = (today.getMonth() + 1).toString().padStart(2, '0')
+    getCalendar(year, month)
+  }, [])
+
+  // 달 이동할 때 달 정보 받음
+  const getDate = (year, month) => {
+    console.log(year, month)
+    getCalendar(`${year}`, `${month.toString().padStart(2, '0')}`)
   }
+
+  const moveDiaryDetail = (date, dayData) => {
+    if (dayData.length > 0) {
+      navigation.navigate("DiaryDetail", {
+        date: date,
+        dayData: dayData[0],
+      })
+    }
+  }
+
   const moveDiaryCreate = () => {
     navigation.navigate("DiaryCreate")
   }
 
   return (
-    <>
-      <Container>
-        <JustifyView>
-          <Title>나의 요리 달력</Title>
-          <BtnContainer>
-            <Btn onPress={moveDiaryCreate}>
-              <Icon name="add" size={24} color="white" />
-            </Btn>
-          </BtnContainer>
-        </JustifyView>
+    <Container>
+      <JustifyView>
+        <Title>나의 요리 달력</Title>
+        <BtnContainer>
+          <Btn onPress={moveDiaryCreate}>
+            <Icon name="add" size={24} color="white" />
+          </Btn>
+        </BtnContainer>
+      </JustifyView>
 
-        <MyCalendar
-          dayComponent={({date, state}) => {
-            let textColor = '#000' // 기본 텍스트 색상
+      <MyCalendar
+        onMonthChange={(month) => {
+          getDate(month.year, month.month)
+        }}
 
-            if (state === 'disabled') {
-              textColor = '#d9e1e8' // 비활성화된 날짜(저번달/다음달 날짜)의 색상
-            } else if (state === 'today') {
-              textColor = 'red'
-            }
+        dayComponent={({date, state}) => {
+          const dayData = dummyfood.filter(food => food.photoDate === date.dateString)
 
-            return (
-              <TouchableOpacity onPress={() => moveDiaryDetail(date)}>
-                <DayStyle isSelected={state === 'selected'}>
-                  <DayNum textColor={textColor} isToday={state === 'today'}>
-                    {date.day}
-                  </DayNum>
-                  {state !== 'disabled' && 
-                    dummyfood.map((food) => {
-                      return food.photoDate === date.dateString ? 
-                      <CalendarImage
+          let textColor = '#000' // 기본 텍스트 색상
+
+          if (state === 'disabled') {
+            textColor = '#d9e1e8' // 비활성화된 날짜(저번달/다음달 날짜)의 색상
+          } else if (state === 'today') {
+            textColor = 'red'
+          }
+
+          return (
+            <TouchableOpacity onPress={() => moveDiaryDetail(date, dayData)}>
+              <DayStyle isSelected={state === 'selected'}>
+                <DayNum textColor={textColor} isToday={state === 'today'}>
+                  {date.day}
+                </DayNum>
+                {state !== 'disabled' && 
+                  dummyfood.map((food) => {
+                    return food.photoDate === date.dateString ? 
+                    <CalendarImage
                       key={food.id}
                       source={{ uri: `${food.photoUrl}` }}
                     /> : null
-                    })
-                  }
-                </DayStyle>
-              </TouchableOpacity>
-            )
-          }}
-        />
-      </Container>
-    </>
+                  })
+                }
+              </DayStyle>
+            </TouchableOpacity>
+          )
+        }}
+      />
+    </Container>
   )
 }
 
--- FRONTEND/mozzi/src/store/DiaryStore.tsx
+++ FRONTEND/mozzi/src/store/DiaryStore.tsx
@@ -1,12 +1,27 @@
 import { create } from 'zustand'
 import axios from '../../axios'
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 const useDiaryStore = create((set) => ({
-  getDiaryProfile: async () => {
+  calendarData: '',
+  getCalendar: async (year, month) => {
+    console.log('년도와 월 잘받았나?', year, month)
+    const token = await AsyncStorage.getItem('accessToken')
     try {
-      const response = await axios.post('mozzi/diary/getmydiary')
+      const response = await axios.get('mozzi/diary/getmydiary', {
+        headers: {
+          Authorization: `Bearer ${token}` // 헤더에 토큰 포함
+        },
+        params: { 
+          foodYear: "2024",
+          foodMonth: "01",
+        }
+      })
+      set({ calendarData: response.data.foods })
+      console.log('캘린더 데이터 잘 받음', response.data)
     } catch (error) {
-      console.error('초기 프로필 정보 보내기 실패', error)
+      console.error('캘린더 데이터 요청 실패', error)
     }
   },
 }))
