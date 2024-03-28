4b61c80a76f09b23cd71f5b529aa466c41b54b26
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2024-03-28 17:02:58 +0900

    feat: Add celery_base

--- BACKEND/mozzi_django_folder/mozzi_django/celery.py
+++ BACKEND/mozzi_django_folder/mozzi_django/celery.py
@@ -1,11 +1,20 @@
-# celery.py (프로젝트 루트 디렉토리에 생성)
+# celery_config.py (프로젝트 루트 디렉토리에 생성)
 
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mozzi_django.settings')
 
-app = Celery('your_project_name')
+app = Celery('mozzi_django')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    'reset_food_today_views': {
+        'task': 'datas.tasks.reset_food_views',
+        'schedule': crontab(hour=17, minute=2),  # 매일 오전 12시 실행
+    },
+}
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
