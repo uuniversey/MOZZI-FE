363883f4ec5e94182a48f6a6a07a18eb54b87076
김상범 <oironese@naver.com>
2024-03-28 16:37:16 +0900

    chore : for switch

--- BACKEND/mozzi/src/main/java/com/a304/mozzi/domain/foodIngredient/service/FoodIngredientService.java
+++ BACKEND/mozzi/src/main/java/com/a304/mozzi/domain/foodIngredient/service/FoodIngredientService.java
@@ -1,2 +1,18 @@
-package com.a304.mozzi.domain.foodIngredient.service;public class FoodIngredientService {
+package com.a304.mozzi.domain.foodIngredient.service;
+
+import com.a304.mozzi.domain.foodIngredient.model.FoodIngredientModel;
+import com.a304.mozzi.domain.foodIngredient.repository.FoodIngredientRepository;
+import com.a304.mozzi.domain.ingredients.model.IngredientsModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FoodIngredientService {
+    private final FoodIngredientRepository foodIngredientRepository;
+    List<FoodIngredientModel> findFoodIngredientModelsByIngredientsModel(IngredientsModel ingredientsModel){
+        return foodIngredientRepository.findFoodIngredientModelsByIngredientsModel(ingredientsModel);
+    }
 }
