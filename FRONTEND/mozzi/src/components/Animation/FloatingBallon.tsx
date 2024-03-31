892687dc7134c127822573f1a334b0ae47361c6c
geumhyun lee <goldbutnew@gmail.com>
2024-03-31 14:03:51 +0900

    feat: add ocr component

--- FRONTEND/mozzi/src/components/Animation/FloatingBallon.tsx
+++ FRONTEND/mozzi/src/components/Animation/FloatingBallon.tsx
@@ -1,56 +1,60 @@
 import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components/native';
-import { Animated, Image } from 'react-native';
+import { Animated, Easing } from 'react-native';
 
-const Ballon = styled(Image)`
+const BalloonImage = styled.Image`
   width: 100px;
   height: 100px;
   opacity: 0.8;
-`
+`;
+
+// Animated.View를 위한 styled-components 생성
+const AnimatedView = styled(Animated.View)`
+  position: absolute;
+  bottom: 0;
+`;
 
 const FloatingBalloon = ({ source, duration = 5000, startDelay = 0 }) => {
-  const moveVertical = useRef(new Animated.Value(0)).current; // 풍선의 수직 시작 위치
-  const moveHorizontal = useRef(new Animated.Value(0)).current; // 풍선의 수평 시작 위치
+  const moveVertical = useRef(new Animated.Value(0)).current;
+  const moveHorizontal = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    // 수직 움직임
-    const verticalUp = Animated.timing(moveVertical, {
-      toValue: -500, // 위로 얼마나 움직일지
-      duration: duration,
-      useNativeDriver: true,
-    });
-
-    // 지그재그 움직임
-    const zigzagMove = Animated.loop(
+    // 지그재그 움직임을 무한 반복
+    Animated.loop(
       Animated.sequence([
         Animated.timing(moveHorizontal, {
-          toValue: 50, // 오른쪽으로 얼마나 움직일지
+          toValue: 50,
           duration: duration / 4,
           useNativeDriver: true,
         }),
         Animated.timing(moveHorizontal, {
-          toValue: -50, // 왼쪽으로 얼마나 움직일지
+          toValue: -50,
           duration: duration / 4,
           useNativeDriver: true,
         }),
-      ]),
-    );
+      ])
+    ).start();
 
-    // 수직과 지그재그 애니메이션 동시 실행
-    Animated.parallel([verticalUp, zigzagMove]).start();
+    const startVerticalAnimation = () => {
+      // 수직 이동 애니메이션
+      moveVertical.setValue(0); // 수직 위치 초기화
+      Animated.timing(moveVertical, {
+        toValue: -800, // 목표 높이
+        duration,
+        useNativeDriver: true,
+      }).start(() => startVerticalAnimation()); // 애니메이션이 완료되면 재귀적으로 재시작
+    };
 
-  }, [moveVertical, moveHorizontal, duration]);
-
+    startVerticalAnimation(); // 수직 애니메이션 시작
+  }, [duration]);
 
   return (
-    <Animated.View
+    <AnimatedView
       style={{
         transform: [{ translateY: moveVertical }, { translateX: moveHorizontal }],
-        position: 'absolute',
-        bottom: 0, // 화면 바닥에서 시작
       }}>
-      <Ballon source={source} />
-    </Animated.View>
+      <BalloonImage source={source} />
+    </AnimatedView>
   );
 };
 
--- FRONTEND/mozzi/src/screens/fridge/FridgeDetailScreen.tsx
+++ FRONTEND/mozzi/src/screens/fridge/FridgeDetailScreen.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { ScrollView, Keyboard, Platform, Text, Alert } from 'react-native';
+import { ScrollView, Keyboard, Platform, Text, Alert, Touchable } from 'react-native';
 import styled from 'styled-components/native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
 import { useNavigation } from '@react-navigation/native';
 
+import FridgeOCR from './FridgeOcr';
 import { Header } from '../../components/Header/Header';
 import { SearchFood } from '../../components/AutoWord/SearchFood';
 import useFridgeStore from '../../store/FridgeStore';
 import note from '../../assets/fridge/note.png';
 import clip from '../../assets/fridge/clip.png';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface FoodItem {
   food: string;
@@ -87,7 +88,7 @@ const SendButton = styled.TouchableOpacity`
   top: 12;
   right: 30;
   bottom: 0; 
-`; 
+`;
 
 const FridgeDetailScreen = ({ route }) => {
   const [text, setText] = useState<FoodItem | null>(null);
@@ -141,6 +142,12 @@ const FridgeDetailScreen = ({ route }) => {
     getMyFoods(storedPos);
   };
 
+  // OCR 관련
+  const handleOcrResult = (text) => {
+    // 처리된 OCR 텍스트를 상태로 설정
+    setText(text);
+  };
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <Header>
@@ -176,7 +183,15 @@ const FridgeDetailScreen = ({ route }) => {
         </Note>
       )}
 
+      {/* 키보드가 열릴 때만 OCR 기능 활성화 버튼을 표시 */}
+      
+
       <InputContainer>
+        {keyboardOpen && (
+          <FridgeOCR 
+            onOcrComplete={handleOcrResult} 
+          />
+        )}
         <SearchFood
           data={allFoods}
           setQuery={setText}
