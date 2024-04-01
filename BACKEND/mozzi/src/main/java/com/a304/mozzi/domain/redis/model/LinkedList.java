08d999767318805c72cf4711032b0b4e3b4a534f
김상범 <oironese@naver.com>
2024-04-01 16:49:47 +0900

    feat : save

--- BACKEND/mozzi/src/main/java/com/a304/mozzi/domain/redis/model/LinkedList.java
+++ BACKEND/mozzi/src/main/java/com/a304/mozzi/domain/redis/model/LinkedList.java
@@ -1,5 +1,15 @@
 package com.a304.mozzi.domain.redis.model;
 
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
 public class LinkedList {
+    RedisTemplate<String, Object> redisTemplate;
+    public void createUserCache(String userId)
+    {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        values.set(userId + ":LinkedList:Head", values);
+    }
+    public String
 }
